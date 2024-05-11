59cac2eb7b90d1486af4d00b295de8cf46dff59f
wilson09 <wilsoncc09@gmail.com>
2024-05-11 20:13:15 -0300

    se sube el componente de la pagina contacto

--- FRONT/DressCode2024/src/app/app.component.ts
+++ FRONT/DressCode2024/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ContactoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'DressCode2024';
--- FRONT/DressCode2024/src/app/app.routes.ts
+++ FRONT/DressCode2024/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'Contacto', component: ContactoComponent },
+];
