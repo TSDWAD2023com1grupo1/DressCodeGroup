e4ec26384f3bba30794e675e999d30dd622ebef3
Noah <noahncassutti@gmail.com>
2024-05-27 23:37:35 -0300

    creacion servicios http

--- FRONT/DressCode2024/src/app/pages/productos/productos.component.ts
+++ FRONT/DressCode2024/src/app/pages/productos/productos.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Producto, productoService } from '../../service/producto.service'; // Importa correctamente
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-productos',
   standalone: true,
-  imports: [],
+  
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './productos.component.html',
-  styleUrl: './productos.component.css'
+  styleUrls: ['./productos.component.css'], // styleUrl -> styleUrls
 })
-export class ProductosComponent {
+export class ProductosComponent implements OnInit {
+  productos: Producto[] = [];
+  @Injectable(productoService : ProductoService)
+  constructor() { }
 
+
+  ngOnInit(): void {
+    this.productoService.obtenerProductos()
+      .subscribe(productos => this.productos = productos);
+  }
 }
