ccb52d97afdce18943cc2af3a02ee6628bf336b1
Elizabeth Chialva <chialvaelizabeth@gmail.com>
2024-05-29 14:07:41 -0300

    creo servicio dashboard

--- FRONT/DressCode2024/src/app/dashboard/dashboard.component.ts
+++ FRONT/DressCode2024/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink}  from '@angular/router';
+import { DashboardService } from '../services/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
+  providers:[DashboardService]
 })
 export class DashboardComponent {
 
